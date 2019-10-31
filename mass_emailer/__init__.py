8347ec0a144885864889e2e48bdcd98e35e9aec6
Alexis Métaireau <alexis@notmyidea.org>
2019-10-31 20:36:45 +0100

    Update the script with new options.

--- mass_emailer/__init__.py
+++ mass_emailer/__init__.py
@@ -9,25 +9,29 @@ from tablib import Dataset
 
 
 def send_emails(templates_folder, dataset, server, sender, tls=False,
-                username=None, password=None, email_field='email', cc=None):
+                username=None, password=None, email_field='email', cc=None,
+                reply_to=None, dry_run=False):
     if cc is None:
         cc = []
     else:
         cc = [cc, ]
 
-    records = Dataset().load(open(dataset).read(), format="tsv")
+    records = Dataset().load(open(dataset).read())
 
     simple_loader = FileSystemLoader(templates_folder)
     env = Environment(loader=simple_loader)
     subject_template = env.get_template("subject")
     body_template = env.get_template("body")
 
-    # Connect to the server
-    server = smtplib.SMTP(server)
-    if tls:
-        server.starttls()
-    if username and password:
-        server.login(username, password)
+    if not dry_run:
+        # Connect to the server
+        server = smtplib.SMTP(server)
+        if tls:
+            print("Initiate TLS")
+            server.starttls()
+        if username and password:
+            print("Login to server")
+            server.login(username, password)
 
     # Now, for each record in the dataset, loop and send emails !
     for record in records.dict:
@@ -38,9 +42,15 @@ def send_emails(templates_folder, dataset, server, sender, tls=False,
         message['From'] = sender
         message['To'] = to
         message['Cc'] = ", ".join(cc)
-        server.sendmail(sender, cc + [to, ],  message.as_string())
-    server.quit()
-
+        if reply_to:
+            message['Reply-To'] = reply_to
+        if dry_run:
+            print(sender, cc, to, text)
+        else:
+            print("Send email to {0]}".format(to))
+            server.sendmail(sender, cc + [to, ],  message.as_string())
+    if not dry_run:
+        server.quit()
 
 def main():
     parser = argparse.ArgumentParser(
@@ -61,6 +71,10 @@ def main():
     parser.add_argument('-f', '--sender', dest='sender',
                         help='Email address you want to use as a sender.',
                         default='massmailer@yopmail.com')
+    
+    parser.add_argument('--reply-to', dest='reply_to',
+                        help='Reply-to email adresse to use',
+                        default=None)
 
     parser.add_argument('--tls', dest='tls', action='store_true',
                         default=False, help='Should connect using TLS.')
@@ -74,11 +88,14 @@ def main():
     parser.add_argument('-e', '--email-field', dest='email_field',
                         help='Field to use as an email field.',
                         default='email')
+    
+    parser.add_argument('--dry-run', dest='dry_run', action='store_true',
+                        default=False, help='Just print what would be sent, do not email it.')
 
     parser.add_argument('--cc', dest='cc',
                         help='Address to put in cc.', default=None)
 
     args = parser.parse_args()
     send_emails(args.templates_folder, args.dataset, args.server, args.sender,
-                args.tls, args.username, args.password,
-                email_field=args.email_field, cc=args.cc)
+                args.tls, args.username, args.password, reply_to=args.reply_to,
+                email_field=args.email_field, cc=args.cc, dry_run=args.dry_run)
